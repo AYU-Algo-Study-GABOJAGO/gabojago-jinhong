e0cfb976099d0b9a04cf9ace17b2d1282e0c0fb0
jangjinhong <honge1122@naver.com>
2024-04-10 16:22:19 +0900

    [2024-04-10] 2주차_페어 문제 풀이 첫번째 해결완료

--- src/week02/queue/Q1966_Jinhong_Jang.java
+++ src/week02/queue/Q1966_Jinhong_Jang.java
@@ -1,27 +1,45 @@
-package week02;
+package week02.queue;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Q1966_Jinhong_Jang {
+    /**
+     * 메모리: 17108 KB  (/ 128000 KB)
+     * 시간:  176 ms    (/ 2000 ms)
+     */
     public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
-        int t = sc.nextInt(); //test case
-        while (t-- > 0) { //3
-            int n = sc.nextInt(); //6 0
-            int m = sc.nextInt(); //6 0
-            for (int i = 0; i < n; i++) {  //6번
-                queue.add(sc.nextInt());
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            Queue<int[]> queue = new LinkedList<>();
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++)
+                queue.add(new int[]{i, Integer.parseInt(st.nextToken())});
+
+            int cnt = 0;
+            while (!queue.isEmpty()) {
+                int max = queue.peek()[1];
+                for (int[] q : queue) max = Math.max(q[1], max);
+
+                int[] poll = queue.poll();
+                if(poll[1] < max) {
+                    queue.offer(poll);
+                }
+                else {
+                    cnt++;
+                    if(poll[0] == m) break;
+                }
             }
 
-            queue.stream().sorted();
-            System.out.println(queue);
+            System.out.println(cnt);
         }
-
     }
 }
